a552d7ec0d394e03dc9a54ac33de5a0f898c7a62
qaqaqa <1149140003@qq.com>
2018-09-13 11:25:16 +0800

    合约明细

--- src/native/routes/Home/Panel3/index.tsx
+++ src/native/routes/Home/Panel3/index.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
 import { TextInput, Text, StyleSheet, View, TouchableOpacity, Slider } from 'react-native'
+import { di } from 'jsmodules';
+import { InstrumentState } from '../../../../stores/bitmex/subscribes';
 var Dimensions = require('Dimensions');
 const width = Dimensions.get("window").width;
 
 class Panel3 extends React.Component<any, any> {
-
+    @di.Inject() InstrumentState: InstrumentState;
     render() {
+        debugger
+        if(this.InstrumentState.instrument){
+            var instrument = this.InstrumentState.instrument[0];
+        }
         return <View style={styles.bgView}>
             <Text style={styles.title}>合约明细</Text>
 
-            <Text style={styles.priceText}>7999.00</Text>
+            <Text style={styles.priceText}>{instrument.lastPriceProtected}</Text>
+            <Text style={styles.priceText}>指数价格：{instrument.fairPrice}/标记价格：{instrument.markPrice}</Text>
             <Text style={styles.textInputTitle}>价格来源：BitMex指数</Text>
-            <Text style={styles.textInputTitle}>BitMex指数价格：100000</Text>
-            <Text style={styles.textInputTitle}>24小时交易量：100000000000</Text>
-            <Text style={styles.textInputTitle}>未平仓合约数量：1000</Text>
-            <Text style={styles.textInputTitle}>资金费率：1000</Text>
-            <Text style={styles.textInputTitle}>合约价值：1000</Text>
+            <Text style={styles.textInputTitle}>BitMex指数价格：{instrument.fairPrice}</Text>
+            <Text style={styles.textInputTitle}>24小时交易量：{instrument.volume24h}</Text>
+            <Text style={styles.textInputTitle}>未平仓合约数量：{instrument.openInterest}</Text>
+            <Text style={styles.textInputTitle}>资金费率：{this.InstrumentState.instrument[66].fundingRate*100}% 1小时内</Text>
+            <Text style={styles.textInputTitle}>合约价值：{this.InstrumentState.instrument[66].lotSize}</Text>
         </View>
     }
 }
